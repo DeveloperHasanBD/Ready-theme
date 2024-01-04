b59b7de8820ed6d235ab022d541bb48995560e83
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2024-01-04 10:18:24 +0600

    Update sec-1.php

--- inc/wp-bakery-shortcode/casi/sec-1.php
+++ inc/wp-bakery-shortcode/casi/sec-1.php
@@ -1,68 +1,137 @@
 <?php
-add_action('vc_before_init', 'redp_contact_sec_1_backend');
-function redp_contact_sec_1_backend()
+add_action('vc_before_init', 'redp_home_sec_1_backend');
+function redp_home_sec_1_backend()
 {
+
     vc_map(
         array(
-            "name" => __("Contact info", "redapple"), // Element name
-            "base" => "redp_contact_sec_1", // Element shortcode
-            'icon' => get_template_directory_uri() . '/octgn-assets/images/vc-icon.png',
-            'description' => 'Dedicated for redapple',
-            "class" => "redapple-cstm",
-            "category" => __('OCT Casi', 'redapple'),
-            'params' => array(
+            "name"          => __("Home main Slider", "redapple"), // Element name
+            "base"          => "redp_home_sec_1", // Element shortcode
+            'icon'          => get_template_directory_uri() . '/assets/images/logo-dark.png',
+            'description'   => 'Dedicated for redapple',
+            "class"         => "redapple-cstm",
+            "category"      => __('Home', 'redapple'),
+            "params"        => array(
                 array(
-                    "type" => "textfield",
-                    "holder" => "div",
-                    "class" => "",
-                    "heading" => __("Title ", "redapple"),
-                    "param_name" => "octgn_casi_s2_title",
-                    "value" => __("", "redapple"),
+                    "type"          => "param_group",
+                    "heading"       => "Home main slider",
+                    "param_name"    => "home_main_slider",
+                    "value"         => "",
+                    "params" => array(
+                        array(
+                            "type"                      => "dropdown",
+                            "heading"                   => "Show Additional Options",
+                            "param_name"                => "show_hide_text_video",
+                            "value"                     => array(
+                                "Default Text slider"   => "yes",
+                                "Choose Video slider"   => "no",
+                            ),
+                        ),
+                        array(
+                            "type"          => "attach_image",
+                            "heading"       => "Slide image",
+                            "param_name"    => "text_slide_image",
+                            "dependency"    => array(
+                                "element"   => "show_hide_text_video",
+                                "value"     => "yes",
+                            ),
+                        ),
+                        array(
+                            "type"          => "textarea",
+                            "heading"       => "Slide text",
+                            "param_name"    => "slide_image_text",
+                            "dependency"    => array(
+                                "element"   => "show_hide_text_video",
+                                "value"     => "yes",
+                            ),
+                        ),
+                        array(
+                            "type"          => "textfield",
+                            "heading"       => "Video URL",
+                            "param_name"    => "video_url",
+                            "dependency"    => array(
+                                "element"   => "show_hide_text_video",
+                                "value"     => "no",
+                            ),
+                        ),
+                        array(
+                            "type"          => "textarea",
+                            "heading"       => "Video text",
+                            "param_name"    => "video_text",
+                            "dependency"    => array(
+                                "element"   => "show_hide_text_video",
+                                "value"     => "no",
+                            ),
+                        ),
+                    ),
                 ),
-            )
+            ),
         )
     );
 }
 
+add_shortcode('redp_home_sec_1', 'redp_home_sec_1_view');
 
-add_shortcode('redp_contact_sec_1', 'redp_contact_sec_1_view');
-
-function redp_contact_sec_1_view($atts)
+function redp_home_sec_1_view($atts)
 
 {
-
     ob_start();
     $atts = shortcode_atts(array(
+        'home_main_slider'     => '',
+    ), $atts, 'redp_home_sec_1');
 
-        'octgn_casi_s2_title' => '',
-
-    ), $atts, 'redp_contact_sec_1');
-
-
-
-    $octgn_casi_s2_title = $atts['octgn_casi_s2_title'] ?? '';
-    // $octgn_casi_s2_logo_ids = $atts['octgn_casi_s2_logos'] ?? '';
-
-    // $octgn_casi_s2_logo_ids_url = wp_get_attachment_image_url($octgn_casi_s2_logo_ids);
-
-    // $items = vc_param_group_parse_atts($atts['octgn_hm_sec_1_sliders_items']);
-
-    // $all_logo_ids = explode(",", $octgn_casi_s2_logo_ids);
-
-    // $logo_id_count = count($all_logo_ids);
-
-
-
-
-
+    $slide_items = vc_param_group_parse_atts($atts['home_main_slider']);
 ?>
+    <!-- home slider start from here  -->
+    <section class="home-slider-main">
+        <div class="swiper home-slider">
+            <div class="swiper-wrapper">
+                <?php
+                foreach ($slide_items as $single_slide) {
+                    $show_hide_text_video   = $single_slide['show_hide_text_video'] ?? '';
 
+
+                    if ($show_hide_text_video == 'yes') {
+                        $text_slide_img_id       = $single_slide['text_slide_image'] ?? '';
+                        $text_slide_img_url      = wp_get_attachment_image_url($text_slide_img_id, 'full');
+                        $slide_image_text        = $single_slide['slide_image_text'] ?? '';
+                ?>
+                        <div class="swiper-slide">
+                            <div class="slide-bg" style="background: url(<?php echo $text_slide_img_url; ?>"> </div>
+                            <div class="slider-cotentbox">
+                                <h2><?php echo $slide_image_text; ?></h2>
+                            </div>
+                        </div>
+                    <?php
+                    } else {
+                        $video_url  = $single_slide['video_url'] ?? '';
+                        $video_text = $single_slide['video_text'] ?? '';
+                    ?>
+                        <div class="swiper-slide video-slide" data-swiper-autoplay="">
+                            <div class="video-bg">
+                                <video autoplay muted loop class="swiper-video">
+                                    <source src="<?php echo $video_url; ?>" type="video/mp4">
+                                </video>
+                                <div class="slider-cotentbox">
+                                    <h2><?php echo $video_text; ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+            <!-- <div class="swiper-pagination"></div> -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </section>
+    <!-- home slider ends here  -->
 
 <?php
 
     $result = ob_get_clean();
 
     return $result;
-
 }
-
